e3df988ec2c64347eae5b51e1968db6a2408e174
veemish <vlikunama@gmail.com>
2019-09-12 17:36:21 +0300

    Check if the inputs are empty issue#3 Alert to fill inputs if the inputs are empty.

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ function CalculateTip(){
         alert ( "Please enter values" );
         return;
     }
+    //check to see id the input is empty or less than or equal to 1//
+    
+    if (numberOfPeople === "" || numberOfPeople <= 1) {
+        numberOfPeople =1;
+        document.getElementById("each").style.display = "none";
+    
+    } else {
+       document.getElementById("each").style.display = "block"; 
+    }
+
+    
 }
