863277fe46783d81d16ca15b167419eac0fe2b17
veemish <vlikunama@gmail.com>
2019-09-13 18:16:08 +0300

    not calculate the bill issue3 javascript not working need to fix it

--- index.js
+++ index.js
@@ -17,6 +17,32 @@ function CalculateTip(){
     } else {
        document.getElementById("each").style.display = "block"; 
     }
+   
+    //calculate tip//
 
-    
+    var total = (billIng * serviceQual) / numberOfPeople;
+
+    //round to two decimal places
+    total = Math.round(total * 100) / 100;
+
+    // next line allowa us to always have two digits after decimal point//
+
+    total = total.toFixed(2);
+
+    //Display the tip//
+
+     document.getElementById("totaltip").style.display = "block";
+       document.getElementById("tip").innerHTML = total;
+
 }
+
+// Hide the tip amount on load//
+   document.getElementById("totalTip").style.display = "none";
+   document.getElementById("each").style.display = "none";
+
+   //click toa call function 
+   document.getElementById("calculate").onclick = function(){
+       CalculateTip();
+   };
+
+   
